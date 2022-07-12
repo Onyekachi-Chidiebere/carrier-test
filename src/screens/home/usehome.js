d7dee2bcfb3bd16850960a7a17f0237110846d6e
Onyekachi-Chidiebere <onyekachisamuel18@gmai.com>
2022-07-12 07:07:38 +0100

    started search function

--- src/screens/home/usehome.js
+++ src/screens/home/usehome.js
@@ -1,10 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import transactions from "../../helper/transactions";
 
 const useHome = () => {
     //initialize data;
     const [data, setdata] = useState([]);
     const [searchQuery, setSearchQuery] = useState('');
+
+    useEffect(()=>{
+        //restructure data into sections;
+        for(let i =0; i< transactions.length; i++){
+            let structuredObject = {};
+            for(let j =0; j<Object.values(transactions[i]).length; j++){
+                if(Object.values(transactions[i][j].includes(searchQuery))){
+                    if(structuredObject[transactions[i].date]){
+                        return  structuredObject[transactions[i].date].push(transactions[i])
+                    }else{
+                        return  structuredObject[transactions[i].date] = [transactions[i]];
+
+                    }
+                }
+            }
+        }
+
+        let structuredList = [];
+        
+    })
 
 
 
