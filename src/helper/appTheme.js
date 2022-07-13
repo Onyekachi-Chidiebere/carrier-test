8f0cc918f80fefc0464cb3a07e8da8494b60ba79
Chidiebere <onyekachisamuel18@gmail.com>
2022-07-13 05:04:02 +0100

    added filters

---
+++ src/helper/appTheme.js
@@ -0,0 +1,14 @@
+const appTheme = {
+  colors: {
+    white: '#FFFFFF',
+    gray: '#A9A9A9',
+    red: '#C62828',
+    lightGreen: '#21FBCC',
+    green: '#23A65E',
+    darkGreen: '#23A65E',
+    darkBlack: '#1E1E1E',
+    lightBlack: '#252525',
+  },
+};
+
+export default appTheme;
--- src/helper/transactions.js
+++ src/helper/transactions.js
@@ -1,5 +1,40 @@
 const transactions = [
-    
+  {
+    date: '7/12/2022',
+    status: 1,
+    amount: '1000',
+    accountNumber: '1223424342',
+    id: 1,
+    type: 0,
+    name: 'Emeka kalu',
+  },
+  {
+    date: '7/12/2022',
+    status: 2,
+    amount: '1000',
+    accountNumber: '1223424342',
+    id: 1,
+    type: 0,
+    name: 'Chuma kalu',
+  },
+  {
+    date: '7/11/2022',
+    status: 3,
+    amount: '1000',
+    accountNumber: '1223424342',
+    id: 1,
+    type: 0,
+    name: 'Chuma kalu',
+  },
+  {
+    date: '7/11/2022',
+    status: 3,
+    amount: '1000',
+    accountNumber: '1223424342',
+    id: 1,
+    type: 0,
+    name: 'Chuma kalu',
+  },
 ];
 
 export default transactions;
