687e923ff50e6e55823b51b953a57afdfd4b5693
Chidiebere <onyekachisamuel18@gmail.com>
2022-07-14 04:55:38 +0100

    added server

--- server.js
+++ server.js
@@ -1,39 +1,94 @@
-const {ApolloServer, gql } = require('apollo-server');
-const { MockList } = require('@graphql-tools/mock');
 const  casual  = require('casual');
+const express = require('express');
+const port = process.env.PORT || 5000;
+const {graphqlHTTP} = require('express-graphql');
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLID,
+  GraphQLString,
+  GraphQLList,
+} = require('graphql');
+const {addMockFunctionToSchema} = require('@graphql-tools/utils');
 
-const typeDefs = gql`
-type Cat {
-    id:ID!
-    date:String!
-    name:String!
-    status:Int!
-    accountNumber:String!
-    type:Int!
-}
-type Query {
-    allCats:[Cat!]!
-}`;
+const app = express();
 
+//TransactionType
+const TranscationType = new GraphQLObjectType({
+  name: 'Transactions',
+  fields: () => ({
+    id: {type: GraphQLID},
+    date: {type: GraphQLString},
+    name: {type: GraphQLString},
+    status: {type: GraphQLString},
+    accountNumber: {type: GraphQLString},
+    type: {type: GraphQLString},
+  }),
+});
 const mocks = {
-    Query: ()=> ({
-        allCats:() => new MockList(20)
-    }),
-    Cat: ()=>({
-        id:()=>casual.uuid,
-        date:() =>casual.random_element(["2022/07/11","2022/07/12","2022/07/13","2022/07/14","2022/07/15"]),
-        status: () => casual.integer(from = 1, to = 3),
-        accountNumber: () => casual.integer(from = 1000000000, to = 9999999999),
-        name: () => casual.full_name,
-        type: () => casual.integer(from = 0, to = 1),
-    }),
-}
+  Query: () => ({
+    transactions: () => new MockList(20),
+  }),
+  Transactions: () => ({
+    id: () => casual.uuid,
+    date: () =>
+      casual.random_element([
+        '2022/07/11',
+        '2022/07/12',
+        '2022/07/13',
+        '2022/07/14',
+        '2022/07/15',
+      ]),
+    status: () => casual.integer((from = 1), (to = 3)),
+    accountNumber: () => casual.integer((from = 1000000000), (to = 9999999999)),
+    name: () => casual.full_name,
+    type: () => casual.integer((from = 0), (to = 1)),
+  }),
+};
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    transactions: {
+      type: new GraphQLList(TranscationType),
 
-
-
-const server = new ApolloServer({
-    typeDefs,mocks
-
+      resolve(parent, args) {
+        let transactions = [];
+        for (let i = 0; i < 20; i++) {
+          transactions.push({
+            id: () => casual.uuid,
+            date: casual.random_element([
+              '2022/07/11',
+              '2022/07/12',
+              '2022/07/13',
+              '2022/07/14',
+              '2022/07/15',
+            ]),
+            status: casual.integer((from = 1), (to = 3)),
+            accountNumber: casual.integer(
+              (from = 1000000000),
+              (to = 9999999999),
+            ),
+            name: casual.full_name,
+            type: casual.integer((from = 0), (to = 1)),
+          });
+        }
+        return transactions;
+      },
+    },
+  },
 });
 
-server.listen().then(({url}) => console.log(`Server is running on ${url}`))
+const schema = new GraphQLSchema({
+  query: RootQuery,
+});
+
+app.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    mocks,
+    graphiql: true,
+  }),
+);
+
+app.listen(port, console.log('app running on port ' + port));
